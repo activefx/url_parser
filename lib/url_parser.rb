61b74948da69e5a2e03f6070833980a50ebd5b82
Matt Solt <mattsolt@gmail.com>
2014-08-10 09:34:54 -0400

    catch errors by default, base valid? on existence of errors

--- lib/url_parser.rb
+++ lib/url_parser.rb
@@ -20,8 +20,11 @@ end
 
 module UrlParser
 
-
   module Error; end
+
+  class InvalidScheme
+    include UrlParser::Error
+  end
 
   def self.call(text, options = {})
     urls = []
@@ -56,15 +59,241 @@ module UrlParser
 
     attr_reader :url, :original_url, :raise_errors
 
+    attr_accessor :errors
+
     def initialize(url, options = {})
-      @schemes = options.fetch(:schemes) { DEFAULT_SCHEMES }
-      @clean = options.fetch(:clean) { false }
-      @original_url = url
-      @url = @clean ? clean(url) : parse(url)
+      @schemes        = options.fetch(:schemes) { DEFAULT_SCHEMES }
+      @clean          = options.fetch(:clean) { false }
+      @raise_errors   = options.fetch(:raise_errors) { false }
+      @errors         = []
+      @original_url   = url
+      @url            = @clean ? clean(url) : parse(url)
+      prepare
     end
 
     def schemes
       Array.wrap(@schemes)
+    end
+
+    def clean!
+      @parser = nil
+      @url = clean(url)
+      @clean = true
+      self
+    end
+
+    def parser
+      tag_errors do
+        @parser ||= Domainatrix.parse(to_s)
+      end
+    end
+
+    def to_s
+      return '' if errors.any?
+      url.to_s
+    end
+
+    def hash(options = {})
+      return nil if errors.any?
+      clean = options.fetch(:clean) { nil }
+      if clean.nil?
+        Digest::SHA1.hexdigest(url.to_s)
+      else
+        Digest::SHA1.hexdigest(
+          clean ? clean(original_url) : parse(original_url)
+        )
+      end
+    end
+
+    def valid?
+      errors.empty?
+    end
+
+    def join(relative_path)
+      return nil if errors.any?
+      UrlParser.new(
+        Addressable::URI.join(url, relative_path).to_s
+      )
+    end
+
+    # URI Components
+
+    def scheme
+      return nil if errors.any?
+      url.scheme
+    end
+
+    def username
+      return nil if errors.any?
+      url.user
+    end
+    alias_method :user, :username
+
+    def password
+      return nil if errors.any?
+      url.password
+    end
+
+    def userinfo
+      return nil if errors.any?
+      url.userinfo
+    end
+
+    def www
+      return nil if errors.any?
+      return nil if parser.subdomain.empty?
+      parts = slice_domain.split('.')
+      parts.first =~ /www?\d*/ ? parts.shift : nil
+    end
+
+    def subdomain
+      return nil if errors.any?
+      return nil if parser.subdomain.empty?
+      parts = slice_domain.split('.')
+      parts.shift if parts.first =~ /www?\d*/
+      parts.compact.join('.')
+    end
+
+    def subdomains
+      return nil if errors.any?
+      return nil if parser.subdomain.empty?
+      [ www, subdomain ].compact.join('.')
+    end
+
+    def domain_name
+      return nil if errors.any?
+      parser.domain.empty? ? nil : parser.domain
+    end
+
+    def domain
+      return nil if errors.any?
+      if parser.domain_with_public_suffix.empty?
+        nil
+      else
+        parser.domain_with_public_suffix
+      end
+    end
+
+    def tld
+      return nil if errors.any?
+      tld = parser.public_suffix
+      tld.empty? ? nil : tld
+    end
+
+    def hostname
+      return nil if errors.any?
+      url.host
+    end
+
+    def port
+      return nil if errors.any?
+      url.port
+    end
+
+    def host
+      return nil if errors.any?
+      name = [ hostname, port ].compact.join(':')
+      name.empty? ? nil : name
+    end
+
+    def origin
+      return nil if errors.any?
+      url.origin == "null" ? nil : url.origin
+    end
+
+    def authority
+      return nil if errors.any?
+      url.authority
+    end
+
+    def site
+      return nil if errors.any?
+      url.site
+    end
+
+    def directory
+      return nil if errors.any?
+      parts = path.split('/')
+      return '/' if parts.empty?
+      parts.pop unless segment.to_s.empty?
+      parts.unshift('') unless parts.first.to_s.empty?
+      parts.compact.join('/')
+    end
+
+    def path
+      return nil if errors.any?
+      url.path
+    end
+
+    def segment
+      return nil if errors.any?
+      path =~ /\/\z/ ? nil : path.split('/').last
+    end
+
+    def filename
+      return nil if errors.any?
+      return 'index.html' if segment.to_s.empty?
+      return '' if suffix.to_s.empty?
+      segment
+    end
+
+    def suffix
+      return nil if errors.any?
+      ext = File.extname(path)
+      ext[0] = '' if ext[0] == '.'
+      ext.empty? ? nil : ext
+    end
+
+    def query
+      return nil if errors.any?
+      url.query
+    end
+
+    def query_values
+      return {} if errors.any?
+      url.query_values.to_h
+    end
+
+    def fragment
+      return nil if errors.any?
+      url.fragment
+    end
+
+    def resource
+      return nil if errors.any?
+      name = [
+        [ segment, query ].compact.join('?'), fragment
+      ].compact.join('#')
+      name.empty? ? nil : name
+    end
+
+    def relative?
+      return nil if errors.any?
+      url.relative?
+    end
+
+    def absolute?
+      return nil if errors.any?
+      url.absolute?
+    end
+
+    def localhost?
+      return nil if errors.any?
+      !!(hostname =~ /(\A|\.)localhost\z/)
+    end
+
+    private
+
+    def slice_domain
+      parser.subdomain.tap{ |s| s.slice!(domain) }
+    end
+
+    def tag_errors
+      yield
+    rescue Exception => error
+      error.extend(UrlParser::Error)
+      @errors << error
+      raise if raise_errors
     end
 
     def parse(url)
@@ -79,196 +308,10 @@ module UrlParser
       end
     end
 
-    def parser
-      tag_errors do
-        @parser ||= Domainatrix.parse(to_s)
-      end
-    end
-
-    def clean!
-      @parser = nil
-      @url = clean(url)
-      @clean = true
-      self
-    end
-
-    def to_s
-      url.to_s
-    end
-
-    def hash(options = {})
-      clean = options.fetch(:clean) { nil }
-      if clean.nil?
-        Digest::SHA1.hexdigest(url.to_s)
-      else
-        Digest::SHA1.hexdigest(
-          clean ? clean(original_url) : parse(original_url)
-        )
-      end
-    end
-
-    def valid?
-      return true if localhost?
-      return false unless schemes.include?(scheme)
-      return false unless hostname =~ /\./
-      true
-    end
-
-    def join(relative_path)
-      UrlParser.new(
-        Addressable::URI.join(url, relative_path).to_s
-      )
-    end
-
-    # URI Components
-
-    def scheme
-      url.scheme
-    end
-
-    def username
-      url.user
-    end
-    alias_method :user, :username
-
-    def password
-      url.password
-    end
-
-    def userinfo
-      url.userinfo
-    end
-
-    def www
-      return nil if parser.subdomain.empty?
-      parts = slice_domain.split('.')
-      parts.first =~ /www?\d*/ ? parts.shift : nil
-    end
-
-    def subdomain
-      return nil if parser.subdomain.empty?
-      parts = slice_domain.split('.')
-      parts.shift if parts.first =~ /www?\d*/
-      parts.compact.join('.')
-    end
-
-    def subdomains
-      return nil if parser.subdomain.empty?
-      [ www, subdomain ].compact.join('.')
-    end
-
-    def domain_name
-      parser.domain.empty? ? nil : parser.domain
-    end
-
-    def domain
-      if parser.domain_with_public_suffix.empty?
-        nil
-      else
-        parser.domain_with_public_suffix
-      end
-    end
-
-    def tld
-      tld = parser.public_suffix
-      tld.empty? ? nil : tld
-    end
-
-    def hostname
-      url.host
-    end
-
-    def port
-      url.port
-    end
-
-    def host
-      name = [ hostname, port ].compact.join(':')
-      name.empty? ? nil : name
-    end
-
-    def origin
-      url.origin == "null" ? nil : url.origin
-    end
-
-    def authority
-      url.authority
-    end
-
-    def site
-      url.site
-    end
-
-    def directory
-      parts = path.split('/')
-      return '/' if parts.empty?
-      parts.pop unless segment.to_s.empty?
-      parts.unshift('') unless parts.first.to_s.empty?
-      parts.compact.join('/')
-    end
-
-    def path
-      url.path
-    end
-
-    def segment
-      path =~ /\/\z/ ? nil : path.split('/').last
-    end
-
-    def filename
-      return 'index.html' if segment.to_s.empty?
-      return '' if suffix.to_s.empty?
-      segment
-    end
-
-    def suffix
-      ext = File.extname(path)
-      ext[0] = '' if ext[0] == '.'
-      ext.empty? ? nil : ext
-    end
-
-    def query
-      url.query
-    end
-
-    def query_values
-      url.query_values.to_h
-    end
-
-    def fragment
-      url.fragment
-    end
-
-    def resource
-      name = [
-        [ segment, query ].compact.join('?'), fragment
-      ].compact.join('#')
-      name.empty? ? nil : name
-    end
-
-    def relative?
-      url.relative?
-    end
-
-    def absolute?
-      url.absolute?
-    end
-
-    def localhost?
-      !!(hostname =~ /(\A|\.)localhost\z/)
-    end
-
-    private
-
-    def slice_domain
-      parser.subdomain.tap{ |s| s.slice!(domain) }
-    end
-
-    def tag_errors
-      yield
-    rescue Exception => error
-      error.extend(UrlParser::Error)
-      raise
+    # Initialize parser to ensure no errors are raised
+    #
+    def prepare
+      parser
     end
 
   end
--- spec/url_parser_spec.rb
+++ spec/url_parser_spec.rb
@@ -33,10 +33,6 @@ describe UrlParser do
       expect(parser.to_s).to eq link
     end
 
-    it "cannot initialize invalid urls" do
-      expect{ UrlParser.new('http:||bra.ziz') }.to raise_error
-    end
-
     it "adds http by default" do
       expect(UrlParser.new('example.com/path').to_s).to eq link
     end
@@ -45,10 +41,12 @@ describe UrlParser do
       expect(UrlParser.new('//example.com/path').to_s).to eq link
     end
 
-    it "any errors raised inherit from UrlParser::Error" do
-      expect{
-        UrlParser.new('http:||bra.ziz')
-      }.to raise_error UrlParser::Error
+    it "cannot initialize invalid urls" do
+      expect(UrlParser.new('http:||bra.ziz').url).to be_nil
+    end
+
+    it "catches errors from invalid urls" do
+      expect(UrlParser.new('http:||bra.ziz').errors).not_to be_empty
     end
 
     context "options" do
@@ -77,6 +75,22 @@ describe UrlParser do
         it "does not clean the url by default" do
           expect(UrlParser.new(link).to_s)
             .to eq PostRank::URI.parse(parser.original_url).to_s
+        end
+
+      end
+
+      context ":raise_errors" do
+
+        it "raises instead of catching errors" do
+          expect{
+            UrlParser.new('http:||bra.ziz', raise_errors: true)
+          }.to raise_error
+        end
+
+        it "any errors raised inherit from UrlParser::Error" do
+          expect{
+            UrlParser.new('http:||bra.ziz', raise_errors: true)
+          }.to raise_error UrlParser::Error
         end
 
       end
@@ -123,10 +137,10 @@ describe UrlParser do
       UrlParser.new(link, clean: false)
     end
 
-    it "tags errors" do
-      parser = UrlParser.new(link, clean: true)
+    it "tags errors when set to raise errors" do
+      parser = UrlParser.new(link, clean: true, raise_errors: true)
       expect(PostRank::URI).to receive(:parse).and_raise(StandardError)
-      expect{ parser.parse(link) }.to raise_error UrlParser::Error
+      expect{ parser.send(:parse, link) }.to raise_error UrlParser::Error
     end
 
   end
@@ -141,9 +155,9 @@ describe UrlParser do
     end
 
     it "tags errors" do
-      parser = UrlParser.new(link, clean: false)
+      parser = UrlParser.new(link, clean: false, raise_errors: true)
       expect(PostRank::URI).to receive(:clean).and_raise(StandardError)
-      expect{ parser.clean(link) }.to raise_error UrlParser::Error
+      expect{ parser.send(:clean, link) }.to raise_error UrlParser::Error
     end
 
   end
@@ -153,15 +167,15 @@ describe UrlParser do
     let(:link) { 'link.to?a=b&utm_source=FeedBurner#stuff' }
 
     it "calls postrank-uri's clean function" do
-      parser = UrlParser.new(link, clean: true)
       expect(Domainatrix).to receive(:parse).with(parser.to_s)
-      parser.parser
+      UrlParser.new(link, clean: true)
     end
 
     it "tags errors" do
-      parser = UrlParser.new(link, clean: false)
       expect(Domainatrix).to receive(:parse).and_raise(StandardError)
-      expect{ parser.parser }.to raise_error UrlParser::Error
+      expect{
+        UrlParser.new(link, clean: false, raise_errors: true)
+      }.to raise_error UrlParser::Error
     end
 
   end
@@ -208,24 +222,12 @@ describe UrlParser do
 
   context "#valid?" do
 
-    it "returns false if the url is invalid" do
-      expect(UrlParser.new('bullshit')).not_to be_valid
+    it "returns true if there are no errors" do
+      expect(UrlParser.new('http://example.com')).to be_valid
     end
 
-    it "returns false if the url scheme is not in the options" do
-      expect(UrlParser.new('telnet://some.com')).not_to be_valid
-    end
-
-    it "returns true if the url scheme is in the options" do
-      expect(UrlParser.new('telnet://some.com', schemes: ['telnet'])).to be_valid
-    end
-
-    it "returns true if the url is valid" do
-      expect(UrlParser.new('http://example.com/')).to be_valid
-    end
-
-    it "returns true for localhost" do
-      expect(UrlParser.new('localhost:5000')).to be_valid
+    it "returns false if there are errors" do
+      expect(UrlParser.new('http:||bra.ziz')).not_to be_valid
     end
 
   end
@@ -273,13 +275,15 @@ describe UrlParser do
 
     let(:parser) { UrlParser.new(link, clean: false) }
 
-    context "when present" do
+    context "when all are present" do
 
       let(:link) do
-        'foo://username:password@ww2.foo.bar.example.com:123/hello/world/there.html?name=ferret#foo'
+        'https://username:password@ww2.foo.bar.example.com:123/hello/world/there.html?name=ferret#foo'
       end
 
-      it { expect(parser.scheme).to eq 'foo' }
+      it { expect(parser.errors).to be_empty }
+      it { expect(parser).to be_valid }
+      it { expect(parser.scheme).to eq 'https' }
       it { expect(parser.username).to eq 'username' }
       it { expect(parser.password).to eq 'password' }
       it { expect(parser.userinfo).to eq 'username:password' }
@@ -292,9 +296,9 @@ describe UrlParser do
       it { expect(parser.hostname).to eq 'ww2.foo.bar.example.com' }
       it { expect(parser.port).to eq 123 }
       it { expect(parser.host).to eq 'ww2.foo.bar.example.com:123' }
-      it { expect(parser.origin).to eq 'foo://ww2.foo.bar.example.com:123' }
+      it { expect(parser.origin).to eq 'https://ww2.foo.bar.example.com:123' }
       it { expect(parser.authority).to eq 'username:password@ww2.foo.bar.example.com:123' }
-      it { expect(parser.site).to eq 'foo://username:password@ww2.foo.bar.example.com:123' }
+      it { expect(parser.site).to eq 'https://username:password@ww2.foo.bar.example.com:123' }
       it { expect(parser.directory).to eq '/hello/world' }
       it { expect(parser.path).to eq '/hello/world/there.html' }
       it { expect(parser.segment).to eq 'there.html' }
@@ -304,15 +308,13 @@ describe UrlParser do
       it { expect(parser.query_values['name']).to eq 'ferret' }
       it { expect(parser.fragment).to eq 'foo' }
       it { expect(parser.resource).to eq 'there.html?name=ferret#foo' }
-
     end
 
-    context "when missing" do
+    context "when none are present" do
 
-      let(:link) do
-        '/'
-      end
+      let(:link) { '/' }
 
+      it { expect(parser.errors).to be_empty }
       it { expect(parser.scheme).to be_nil }
       it { expect(parser.username).to be_nil }
       it { expect(parser.password).to be_nil }
@@ -333,6 +335,72 @@ describe UrlParser do
       it { expect(parser.path).to eq '/' }
       it { expect(parser.segment).to be_nil }
       it { expect(parser.filename).to eq 'index.html' }
+      it { expect(parser.suffix).to be_nil }
+      it { expect(parser.query).to be_nil }
+      it { expect(parser.query_values['name']).to be_nil }
+      it { expect(parser.fragment).to be_nil }
+      it { expect(parser.resource).to be_nil }
+
+    end
+
+    context "when empty" do
+
+      let(:link) { '' }
+
+      it { expect(parser.errors).to be_empty }
+      it { expect(parser.scheme).to be_nil }
+      it { expect(parser.username).to be_nil }
+      it { expect(parser.password).to be_nil }
+      it { expect(parser.userinfo).to be_nil }
+      it { expect(parser.www).to be_nil }
+      it { expect(parser.subdomain).to be_nil }
+      it { expect(parser.subdomains).to be_nil }
+      it { expect(parser.domain_name).to be_nil }
+      it { expect(parser.domain).to be_nil }
+      it { expect(parser.tld).to be_nil }
+      it { expect(parser.hostname).to be_nil }
+      it { expect(parser.port).to be_nil }
+      it { expect(parser.host).to be_nil }
+      it { expect(parser.origin).to be_nil }
+      it { expect(parser.authority).to be_nil }
+      it { expect(parser.site).to be_nil }
+      it { expect(parser.directory).to eq '/' }
+      it { expect(parser.path).to eq '' }
+      it { expect(parser.segment).to be_nil }
+      it { expect(parser.filename).to eq 'index.html' }
+      it { expect(parser.suffix).to be_nil }
+      it { expect(parser.query).to be_nil }
+      it { expect(parser.query_values['name']).to be_nil }
+      it { expect(parser.fragment).to be_nil }
+      it { expect(parser.resource).to be_nil }
+
+    end
+
+    context "when invalid" do
+
+      let(:link) { 'http://#content-zone' }
+
+      it { expect(parser.errors).not_to be_empty }
+      it { expect(parser.scheme).to be_nil }
+      it { expect(parser.username).to be_nil }
+      it { expect(parser.password).to be_nil }
+      it { expect(parser.userinfo).to be_nil }
+      it { expect(parser.www).to be_nil }
+      it { expect(parser.subdomain).to be_nil }
+      it { expect(parser.subdomains).to be_nil }
+      it { expect(parser.domain_name).to be_nil }
+      it { expect(parser.domain).to be_nil }
+      it { expect(parser.tld).to be_nil }
+      it { expect(parser.hostname).to be_nil }
+      it { expect(parser.port).to be_nil }
+      it { expect(parser.host).to be_nil }
+      it { expect(parser.origin).to be_nil }
+      it { expect(parser.authority).to be_nil }
+      it { expect(parser.site).to be_nil }
+      it { expect(parser.directory).to be_nil }
+      it { expect(parser.path).to be_nil }
+      it { expect(parser.segment).to be_nil }
+      it { expect(parser.filename).to be_nil }
       it { expect(parser.suffix).to be_nil }
       it { expect(parser.query).to be_nil }
       it { expect(parser.query_values['name']).to be_nil }
